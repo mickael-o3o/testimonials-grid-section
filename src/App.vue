c0e8a4f9d667b8262171158e0cd42c150136f49b
mickael-o3o <mickael.00@outlook.com>
2025-06-21 15:41:39 +0300

    finished the project

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,77 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import TestiminialsCard from './components/TestiminialsCard.vue'
+import testimonialsData from '@/data/testimonialsData.json'
+</script>
 
 <template>
-  <main>hello c:</main>
+  <main class="p-main">
+    <img class="p-main__quotation-image" src="/images/bg-pattern-quotation.svg" alt="" />
+    <TestiminialsCard
+      v-for="testimonial in testimonialsData"
+      :key="testimonial.id"
+      :background="testimonial.background"
+      :text-color-black="testimonial.textColorBlack"
+      :user-picture="testimonial.userPicture"
+      :user-name="testimonial.name"
+      :message="testimonial.message"
+      :message-details="testimonial.messageDetails"
+      :col-span="testimonial.colSpan"
+      :row-span="testimonial.rowSpan"
+    />
+  </main>
+  <footer class="p-footer">
+    <p class="p-footer__attribution">
+      Challenge by
+      <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>.
+      Coded by <a href="#">mickael-o3o</a>.
+    </p>
+  </footer>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.p-main {
+  padding-block-start: 5.69rem;
+  display: flex;
+  flex-direction: column;
+  gap: 2.5rem;
+  inline-size: 100%;
+  max-inline-size: 85.5rem;
+  margin: 0 auto;
+
+  @media (min-width: 62.5rem) {
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr 1fr;
+    gap: 2.5rem;
+    row-gap: 2rem;
+    padding-block-start: 17.7rem;
+  }
+
+  &__quotation-image {
+    position: absolute;
+    inset-block-start: 28.5%;
+    inset-inline-start: 37.5%;
+    opacity: 0;
+
+    @media (min-width: 62.5rem) {
+      opacity: 0.3;
+    }
+  }
+}
+
+.p-footer {
+  font-family: monospace;
+  margin: 8rem auto;
+  inline-size: fit-content;
+
+  a {
+    color: var(--color-purple-500);
+  }
+
+  &__attribution {
+    border: 0.2rem solid var(--color-grey-400);
+    margin: 1rem;
+    padding: 1rem;
+    text-align: center;
+  }
+}
+</style>
---
+++ src/components/TestiminialsCard.vue
@@ -0,0 +1,139 @@
+<script setup lang="ts">
+interface Props {
+  background: string
+  textColorBlack?: boolean
+  userPicture: string
+  userName: string
+  message: string
+  messageDetails: string
+  colSpan?: string
+  rowSpan?: string
+}
+
+const { background, textColorBlack, userPicture, userName, colSpan, rowSpan } = defineProps<Props>()
+</script>
+
+<template>
+  <article
+    class="c-testimoials-card"
+    :style="{ background: background, gridColumn: colSpan, gridRow: rowSpan }"
+  >
+    <header
+      class="c-testimoials-card__header"
+      :class="{ 'c-testimoials-card__header--black': textColorBlack }"
+    >
+      <img
+        :src="userPicture"
+        alt="user pitcure"
+        class="c-testimoials-card__user-image"
+        :class="{ 'c-testimoials-card__user-image--black': textColorBlack }"
+        width="56px"
+        height="56px"
+      />
+      <section class="c-testimoials-card__user-identity">
+        <h3 class="c-testimoials-card__user-name">{{ userName }}</h3>
+        <p
+          class="c-testimoials-card__user-status"
+          :class="{ 'c-testimoials-card__user-status--black': textColorBlack }"
+        >
+          Verified Graduate
+        </p>
+      </section>
+    </header>
+    <section class="c-testimoials-card__body">
+      <p
+        class="c-testimoials-card__message"
+        :class="{ 'c-testimoials-card__message--black': textColorBlack }"
+      >
+        {{ message }}
+      </p>
+      <p
+        class="c-testimoials-card__message-details"
+        :class="{ 'c-testimoials-card__message-details--black': textColorBlack }"
+      >
+        {{ messageDetails }}
+      </p>
+    </section>
+  </article>
+</template>
+
+<style scoped lang="scss">
+.c-testimoials-card {
+  background: var(--color-purple-500);
+  color: var(--color-white);
+  padding: 2.4rem;
+  padding-block-start: 2.3rem;
+  border-radius: 0.4rem;
+  box-shadow: 2rem 5rem 2rem var(--color-grey-200);
+  max-inline-size: 40rem;
+  margin: 0 auto;
+
+  @media (min-width: 62.5rem) {
+    max-inline-size: 100%;
+  }
+
+  &__header {
+    margin-block-end: 1.2rem;
+    display: flex;
+    align-items: center;
+    gap: 1.1rem;
+
+    &--black {
+      color: var(--color-grey-500);
+    }
+  }
+
+  &__user-image {
+    border: 0.2rem solid var(--color-purple-300);
+    border-radius: 100%;
+    inline-size: 2.4rem;
+    block-size: auto;
+
+    &--black {
+      border: 0.3rem solid var(--color-purple-50);
+    }
+  }
+
+  &__user-identity {
+    display: flex;
+    flex-direction: column;
+    gap: 0.15rem;
+  }
+
+  &__user-name {
+    font-size: 1rem;
+  }
+
+  &__user-status {
+    font-size: 0.85rem;
+    color: var(--color-grey-200);
+
+    &--black {
+      color: var(--color-grey-400);
+    }
+  }
+
+  &__body {
+    margin-inline-start: 0.2rem;
+  }
+
+  &__message {
+    font-family: var(--font-family-bold);
+    font-size: 1.5rem;
+    margin-block-end: 1.2rem;
+
+    &--black {
+      color: var(--color-grey-500);
+    }
+  }
+
+  &__message-details {
+    line-height: 1.4;
+    color: var(--color-grey-100);
+
+    &--black {
+      color: var(--color-grey-400);
+    }
+  }
+}
+</style>
